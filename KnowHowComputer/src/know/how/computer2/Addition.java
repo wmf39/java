c7919d9358b588792ae93b7c5cfffc514d6c98e2
domino <itf2@e.mail.de>
2025-09-25 09:31:21 +0200

    clear warnings

--- KnowHowComputer/src/know/how/computer2/Addition.java
+++ KnowHowComputer/src/know/how/computer2/Addition.java
@@ -17,12 +17,12 @@ public class Addition implements Software {
 	public HashMap<Integer, Triple<Command, Integer, Integer>> getProgram() {
 		HashMap<Integer, Triple<Command, Integer, Integer>> program = new HashMap<>();
 		// storage address; command, source, target
-		program.put(1, new Triple(Command.SPC, null, 4));
-		program.put(2, new Triple(Command.INC, null, 1));
-		program.put(3, new Triple(Command.DEC, null, 2));
-		program.put(4, new Triple(Command.EQZ, null, 2));
-		program.put(5, new Triple(Command.SPC, null, 2));
-		program.put(6, new Triple(Command.STP, null, null));
+		program.put(1, new Triple<>(Command.SPC, null, 4));
+		program.put(2, new Triple<>(Command.INC, null, 1));
+		program.put(3, new Triple<>(Command.DEC, null, 2));
+		program.put(4, new Triple<>(Command.EQZ, null, 2));
+		program.put(5, new Triple<>(Command.SPC, null, 2));
+		program.put(6, new Triple<>(Command.STP, null, null));
 		return program;
 	}
 }
